77f956e3be95bb1b9f3fb5452e510f779c6a3e4f
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-02-01 17:24:35 +0300

    JFontSizePanel/

---
+++ Chapter.16/JFontSizePanel/JFontSizePanel.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+
+import java.awt.*;
+
+public class JFontSizePanel extends JPanel{
+    private String s = "Kelvin";
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        int x = 10;
+        int y = 20;
+        for(int i = 6; i < 21; ++i)
+        {
+            g.setFont(new Font("Arial",Font.PLAIN,i));
+            g.drawString(s,x,y);
+            y += g.getFontMetrics().getHeight();
+        }
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JFontSizePanel jfsp = new JFontSizePanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(jfsp);
+       // jfsp.repaint();
+        frame.setBounds(300,300,300,300);
+        frame.setVisible(true);
+    }
+}
